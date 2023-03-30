c98d490acc34c50c7740c5a602b97d0e728a303d
Lucas Dominique <lucasdominique99@gmail.com>
2023-03-30 15:52:57 +0200

    changed arg to const nd used reference

--- MessageHandler.cpp
+++ MessageHandler.cpp
@@ -7,7 +7,7 @@ void MessageHandler::HandleMessage(int socketId, const std::string &msg) {
     TcpListener::Send(socketId, msg);
 }
 
-void MessageHandler::numericReply(int client_fd, std::string numeric, std::string message)
+void MessageHandler::numericReply(int client_fd, const std::string& numeric, const std::string& message)
 {
 	std::string reply = ":127.0.0.1 " + numeric + " " + message + "\r\n";
 	TcpListener::Send(client_fd, reply);
--- MessageHandler.hpp
+++ MessageHandler.hpp
@@ -11,7 +11,7 @@
 class MessageHandler {
 public:
     static void HandleMessage(int socketId, const std::string &msg);
-	static void	numericReply(int client_fd, std::string numeric, std::string message);
+	static void	numericReply(int client_fd, const std::string& numeric, const std::string& message);
 };
 
 
