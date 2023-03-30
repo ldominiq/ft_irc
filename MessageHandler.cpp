ae464af48c1e46aeb6f16fa12cb3fc8dbd77a2fc
Samuel Bars <sbars@c1r7s5.42lausanne.ch>
2023-03-30 16:09:22 +0200

    privmsg got more features, send_to_client too, mode response and lots of debugging messages

--- MessageHandler.cpp
+++ MessageHandler.cpp
@@ -13,10 +13,23 @@ void MessageHandler::numericReply(int client_fd, std::string numeric, std::strin
 	TcpListener::Send(client_fd, reply);
 }
 
-void MessageHandler::send_to_client(std::string &dest, std::string message)
+void MessageHandler::send_to_client(std::string sender, std::vector<std::string> &params, TcpListener *SERV)
 {
+	std::string message = params[1];
+	// concatenate params 1 to n into string message
+	if (params.size() > 2) {
+		for (std::vector<std::string>::iterator it = params.begin(); it != params.end(); it++)
+			message += *it;
+	}
+
 	std::string trimmed = message.substr(1); // REMOVE the ":" prefix
 	// FIND CLIENT USING NICKNAME
-	std::cout << "send_to_client got " << trimmed << " to send to " << dest;
-	// SEND MESSAGE TO CLIENT
+	Client&	client = SERV->get_client(params[0]);
+	std::cout << client.get_fd() << std::endl;
+	// SEND REFORMATED MESSAGE TO CLIENT
+	std::cout << "message " + message + "$" << std::endl;
+	std::string reform = ":" + sender + " PRIVMSG " + client.get_nick() + " " + message + "\r\n";
+
+	std::cout << reform << std::endl;
+	MessageHandler::HandleMessage(client.get_fd(), reform);
 }
--- Numeric_replies.hpp
+++ Numeric_replies.hpp
@@ -5,11 +5,11 @@
 #ifndef FT_IRC_NUMERIC_REPLIES_HPP
 #define FT_IRC_NUMERIC_REPLIES_HPP
 
-#define user_id(nickname, username) (":" + nickname + "!" + username + "@localhost")
+#define user_id(nickname, username) (nickname + "!" + username + "@localhost")
 #define RPL_WELCOME(user_id, nick) (":localhost 001 " + nick + " :Welcome to the COOL IRC Network, " + user_id + "\r\n")
 #define RPL_YOURHOST(nick) (":localhost 002 " + nick + " :Your host is localhost, running version DEV.1\r\n")
 #define RPL_CREATED(datetime) (":localhost 003 " + nick + " :This server was created " + datetime + "\r\n")
-#define RPL_MYINFO() (":localhost 004 <servername> <version> <available user modes>" "<available channel modes> [<channel modes with a parameter>]\r\n")
+#define RPL_MYINFO() (":localhost 004 gigacoolchat 0.0.1 +i +i\r\n")
 
 #define RPL_MOTDSTART(client) (":localhost 375 " + client + " :- localhost Message of the day - \r\n")
 #define RPL_MOTD(client, motd_line) (":localhost 372 " + client + " :" + motd_line + "\r\n")
